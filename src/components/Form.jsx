d32a1f7aaa72723273b9dc89411ab2ee97552af0
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2023-09-16 22:41:38 +0500

    adding google form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,7 +14,6 @@ import {
 import Confetti from 'react-confetti';
 
 const Form = () => {
-
     const countries = [
         "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia",
         "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus",
@@ -44,20 +43,55 @@ const Form = () => {
     const [showConfetti, setShowConfetti] = useState(false);
     const [hasRelativeInCYC, setHasRelativeInCYC] = useState(false);
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [formData, setFormData] = useState({});
 
-    const handleRelativeChange = (value) => {
-        if (value === "yes") {
-            setHasRelativeInCYC(true);
-        } else {
-            setHasRelativeInCYC(false);
-        }
+    const handleInputChange = (name, value) => {
+        setFormData(prevState => ({ ...prevState, [name]: value }));
     }
 
-    const handleSubmit = (event) => {
+    const handleRelativeChange = (value) => {
+        setHasRelativeInCYC(value === "yes");
+        handleInputChange('hasRelativeInCYC', value);
+    }
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        setIsSubmitted(true);
-        setShowConfetti(true);
-        // Handle your form submission logic here if needed
+
+        const googleFormURL = 'https://docs.google.com/forms/d/e/1FAIpQLSfpFtt02i8BHXEN0Ggwn92KHr1777_UCoLhZ51WyMXyUmCa7g/formResponse';
+
+        const formEntries = {
+            "entry.2158446": formData.name,
+            "entry.2040070596": formData.fatherName,
+            "entry.450925682": formData.cnic,
+            "entry.1729225793": formData.email,
+            "entry.2127968864": formData.whatsappNo,
+            "entry.1052229299": formData.college,
+            "entry.1369234903": formData.country,
+            "entry.472403008": formData.hasRelativeInCYC,
+            "entry.1060629598": formData.relativeName,
+            "entry.1494358699": formData.relation,
+            "entry.1887889065": formData.relativePostalAddress,
+            "entry.986365769": formData.workedForNGO,
+            "entry.1110019471": formData.planningToMove
+        };
+
+        const urlEncodedData = new URLSearchParams(formEntries);
+
+        try {
+            await fetch(googleFormURL, {
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: urlEncodedData.toString()
+            });
+
+            setIsSubmitted(true);
+            setShowConfetti(true);
+        } catch (error) {
+            console.error("Error submitting form: ", error);
+        }
     }
 
     return (
@@ -74,32 +108,32 @@ const Form = () => {
                     <Stack spacing={4}>
                         <FormControl isRequired>
                             <FormLabel>Name</FormLabel>
-                            <Input placeholder="Enter your name" />
+                            <Input placeholder="Enter your name" onChange={(e) => handleInputChange('name', e.target.value)} />
                         </FormControl>
 
                         <FormControl isRequired>
                             <FormLabel>Father Name</FormLabel>
-                            <Input placeholder="Enter your father's name" />
+                            <Input placeholder="Enter your father's name" onChange={(e) => handleInputChange('fatherName', e.target.value)} />
                         </FormControl>
 
                         <FormControl isRequired>
                             <FormLabel>CNIC/Passport</FormLabel>
-                            <Input placeholder="Enter your CNIC or Passport number" />
+                            <Input placeholder="Enter your CNIC or Passport number" onChange={(e) => handleInputChange('cnic', e.target.value)} />
                         </FormControl>
 
                         <FormControl isRequired>
                             <FormLabel>Email</FormLabel>
-                            <Input placeholder="Enter your email" type="email" />
+                            <Input placeholder="Enter your email" type="email" onChange={(e) => handleInputChange('email', e.target.value)} />
                         </FormControl>
 
                         <FormControl isRequired>
                             <FormLabel>Whatsapp No.</FormLabel>
-                            <Input placeholder="Enter your Whatsapp number" />
+                            <Input placeholder="Enter your Whatsapp number" onChange={(e) => handleInputChange('whatsappNo', e.target.value)} />
                         </FormControl>
 
                         <FormControl isRequired>
                             <FormLabel>Country</FormLabel>
-                            <Select placeholder="Select your country">
+                            <Select placeholder="Select your country" onChange={(e) => handleInputChange('country', e.target.value)}>
                                 {countries.map(country => (
                                     <option key={country} value={country}>{country}</option>
                                 ))}
@@ -108,7 +142,7 @@ const Form = () => {
 
                         <FormControl isRequired>
                             <FormLabel>Do you have a relative in Commonwealth Youth Council(CYC)?</FormLabel>
-                            <RadioGroup onChange={value => handleRelativeChange(value)}>
+                            <RadioGroup onChange={(value) => setHasRelativeInCYC(value === "yes")}>
                                 <Stack direction="row">
                                     <Radio value="yes">Yes</Radio>
                                     <Radio value="no">No</Radio>
@@ -120,22 +154,24 @@ const Form = () => {
                             <>
                                 <FormControl isRequired>
                                     <FormLabel>Name of relative</FormLabel>
-                                    <Input placeholder="Enter relative's name" />
+                                    <Input placeholder="Enter relative's name" onChange={(e) => handleInputChange('relativeName', e.target.value)} />
                                 </FormControl>
+
                                 <FormControl isRequired>
                                     <FormLabel>Relation</FormLabel>
-                                    <Input placeholder="Enter relation" />
+                                    <Input placeholder="Enter relation" onChange={(e) => handleInputChange('relation', e.target.value)} />
                                 </FormControl>
+
                                 <FormControl isRequired>
                                     <FormLabel>Postal address of relative</FormLabel>
-                                    <Input placeholder="Enter postal address" />
+                                    <Input placeholder="Enter postal address" onChange={(e) => handleInputChange('relativePostalAddress', e.target.value)} />
                                 </FormControl>
                             </>
                         )}
 
                         <FormControl isRequired>
                             <FormLabel>Have you ever worked for an International NGO?</FormLabel>
-                            <RadioGroup>
+                            <RadioGroup onChange={(value) => handleInputChange('workedForNGO', value)}>
                                 <Stack direction="row">
                                     <Radio value="yes">Yes</Radio>
                                     <Radio value="no">No</Radio>
@@ -145,7 +181,7 @@ const Form = () => {
 
                         <FormControl isRequired>
                             <FormLabel>Are you planning to move to England, Scotland, Wales, or Northern Ireland after graduation?</FormLabel>
-                            <RadioGroup>
+                            <RadioGroup onChange={(value) => handleInputChange('planningToMove', value)}>
                                 <Stack direction="row">
                                     <Radio value="yes">Yes</Radio>
                                     <Radio value="no">No</Radio>
@@ -157,8 +193,9 @@ const Form = () => {
                         <Button mt={4} colorScheme="blue" type="submit">Submit</Button>
                     </Stack>
                 </form>
-            )}
-        </Box>
+            )
+            }
+        </Box >
     );
 }
 
